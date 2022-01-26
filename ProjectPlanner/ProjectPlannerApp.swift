a0fbd6f76dcc606d34a54151843ec1ff5e245524
Austin Suarez <austinmsuarez@gmail.com>
2022-01-25 20:29:56 -0600

    Initial Commit

---
+++ ProjectPlanner/ProjectPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPlannerApp.swift
+//  ProjectPlanner
+//
+//  Created by Austin Suarez on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
